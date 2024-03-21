1c6d3478bfe0a1910968d98b6066846ad46a154a
TimJoePat <TimJoePat88@cox.net>
2024-03-21 00:18:57 -0700

    39 Calling a server action for a client - Moraco Editor working

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import type { Snippets } from "@prisma/client";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippets;
@@ -9,9 +12,30 @@ interface SnippetEditFormProps {
 export default function SnippetEditForm({
   snippet,
 }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
+  const editSnippetAction = actions.editSnippet.bind(
+    null,
+    snippet.id,
+    code
+  );
   return (
     <div>
-      Client component has snippet with title {snippet.title}
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        onChange={handleEditorChange}
+      />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 boarder rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
